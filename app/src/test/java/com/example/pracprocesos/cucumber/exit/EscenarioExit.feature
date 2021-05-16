aa7e6b407e36df068aeaca21321fca3af278ae62
Carlitosh636 <61882229+Carlitosh636@users.noreply.github.com>
2021-05-16 18:48:53 +0200

    test HdU 1

--- app/src/test/java/com/example/pracprocesos/cucumber/exit/EscenarioExit.feature
+++ app/src/test/java/com/example/pracprocesos/cucumber/exit/EscenarioExit.feature
@@ -1,7 +1,7 @@
 Feature: Button to leave an unfinished game.
 	Scenario: Player wants to leave the game prematurely.
     	Given the player is in a game "1"
-        When they push the exit button and confirm
+        When they push the exit button and confirm "2"
         Then the game cancels the current playthrough and returns to the main menu "2"
     Scenario: Player pushes the exit button accidentaly.
     	Given the player pushing the button without noticing "2"
--- app/src/test/java/com/example/pracprocesos/cucumber/exit/exitTest.java
+++ app/src/test/java/com/example/pracprocesos/cucumber/exit/exitTest.java
@@ -8,33 +8,47 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 public class exitTest {
+    private boolean enPartida,confirmacion;
+    private String aux="";
     @Given("^the player is in a game \"([^\"]*)\"$")
     public void the_player_is_in_a_game_something(String strArg1) throws Throwable {
-        throw new PendingException();
+        enPartida=true;
+        confirmacion=false;
+        aux=strArg1;
     }
 
     @Given("^the player pushing the button without noticing \"([^\"]*)\"$")
     public void the_player_pushing_the_button_without_noticing_something(String strArg1) throws Throwable {
-        throw new PendingException();
+        enPartida=true;
+        confirmacion=false;
+        aux=strArg1;
     }
 
-    @When("^they push the exit button and confirm$")
-    public void they_push_the_exit_button_and_confirm() throws Throwable {
-        throw new PendingException();
+    @When("^they push the exit button and confirm \"([^\"]*)\"$")
+    public void they_push_the_exit_button_and_confirm(String strArg1) throws Throwable {
+        enPartida=true;
+        confirmacion=true;
+        aux=strArg1;
     }
 
     @When("^they push the button$")
     public void they_push_the_button() throws Throwable {
-        throw new PendingException();
+        enPartida=true;
+        confirmacion=false;
     }
 
     @Then("^the game cancels the current playthrough and returns to the main menu \"([^\"]*)\"$")
     public void the_game_cancels_the_current_playthrough_and_returns_to_the_main_menu_something(String strArg1) throws Throwable {
-        throw new PendingException();
+        if(aux.equals(strArg1) && confirmacion==true){
+            enPartida=false;
+            System.out.println("El jugador ha salido correctamente de la partida");
+        }
     }
 
     @Then("^a confirmation screen pops up asking if they are sure about quitting. \"([^\"]*)\"$")
     public void a_confirmation_screen_pops_up_asking_if_they_are_sure_about_quitting_something(String strArg1) throws Throwable {
-        throw new PendingException();
+        if(confirmacion==false){
+            System.out.println("Se le muestra al jugador correctamente la pantalla de confirmación");
+        }
     }
 }
